5a00577b89ae50bd02e5f437d751adde39115edf
janmichek <jan@janmichek.cz>
2022-04-22 15:33:40 +0200

    update webp minimizing solution

--- components/AppImage.vue
+++ components/AppImage.vue
@@ -1,20 +1,8 @@
 <template>
-  <picture>
-    <source
-      :srcset="require(`../assets/images/${image}?webp`)"
-      :width="width"
-      :alt="alt"
-      type="image/webp">
-    <source
-      :srcset="require(`../assets/images/${image}`)"
-      :width="width"
-      :alt="alt"
-      type="image/jpeg">
-    <img
-      :src="require(`../assets/images/${image}`)"
-      :width="width"
-      :alt="alt">
-  </picture>
+  <img
+    :src="require(`../assets/images/${image}?webp`)"
+    :width="width"
+    :alt="alt">
 </template>
 
 <script>
